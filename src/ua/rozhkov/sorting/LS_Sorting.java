cc940599c36b743348c0e924ea21346f86615942
PsyhoBelka <Fghjrcbvfwbz88>
2019-02-04 15:39:20 +0200

    change bubble sort algorithm improvement bubble sort done

--- src/ua/rozhkov/sorting/LS_Sorting.java
+++ src/ua/rozhkov/sorting/LS_Sorting.java
@@ -5,44 +5,42 @@ import java.util.Random;
 
 public class LS_Sorting {
 
-    static int[] arrayForSort = new int[20];
+    static int[] arrayForSort = new int[10];
 
     public static void main(String[] args) {
-        init_array();
-        System.out.println("Array to sort:");
-        System.out.println(Arrays.toString(arrayForSort));
-        System.out.println("-------------------------");
 
-        Multisort multisort = new Multisort();
+        MultiSort multisort = new MultiSort();
 
         System.out.println("---Bubble sort---");
+        init_array();
         System.out.println(Arrays.toString(multisort.bubbleSort(arrayForSort)));
-        System.out.println("Time for sorting: " + multisort.sortDuration);
+        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
         System.out.println("-----------------");
 
         System.out.println("---Improvement bubble sort---");
-        multisort.bubbleSortImpr(arrayForSort);
-        System.out.println("Time for sorting: " + multisort.sortDuration);
+        init_array();
+        System.out.println(Arrays.toString(multisort.bubbleSortImpr(arrayForSort)));
+        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
         System.out.println("-----------------------------");
 
         System.out.println("---Selection sort---");
         multisort.selectionSort(arrayForSort);
-        System.out.println("Time for sorting: " + multisort.sortDuration);
+        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
         System.out.println("--------------------");
 
         System.out.println("---Insertion sort---");
         multisort.insertionSort(arrayForSort);
-        System.out.println("Time for sorting: " + multisort.sortDuration);
+        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
         System.out.println("--------------------");
 
         System.out.println("---Merge sort---");
         multisort.mergeSort(arrayForSort);
-        System.out.println("Time for sorting: " + multisort.sortDuration);
+        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
         System.out.println("----------------");
 
         System.out.println("---Quick sort---");
         multisort.quickSort(arrayForSort);
-        System.out.println("Time for sorting: " + multisort.sortDuration);
+        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
         System.out.println("----------------");
 
 
@@ -53,5 +51,8 @@ public class LS_Sorting {
         for (int i = 0; i < arrayForSort.length; i++) {
             arrayForSort[i] = new Random().nextInt(100);
         }
+        System.out.println("Array to sort:");
+        System.out.println(Arrays.toString(arrayForSort));
+        System.out.println("...SORTING...");
     }
 }
--- src/ua/rozhkov/sorting/MultiSort.java
+++ src/ua/rozhkov/sorting/MultiSort.java
@@ -3,37 +3,31 @@ package ua.rozhkov.sorting;
 import java.time.Duration;
 import java.time.LocalTime;
 
-public class Multisort {
-    LocalTime localTimeStart;
-    LocalTime localTimeEnd;
+public class MultiSort {
+    private LocalTime localTimeStart;
+    private LocalTime localTimeEnd;
     public long sortDuration;
 
     public int[] bubbleSort(int[] arrayToSort) {
+
         int tmp;
-        int sorted = 0;
 
+        if (arrayToSort == null) throw new IllegalArgumentException();
+        if (arrayToSort.length == 0) throw new IllegalArgumentException();
+        if (arrayToSort.length == 1) return arrayToSort;
+
+        int i = 0;
         localTimeStart = LocalTime.now();
-
-        while (sorted < arrayToSort.length) {
-            for (int i = 0; i < arrayToSort.length - 1; i++) {
-                if (arrayToSort[i] < arrayToSort[i + 1]) {
-                    break;
-                } else {
-                    sorted++;
-                }
-            }
-
-            if (sorted == arrayToSort.length) {
-                return arrayToSort;
+        while (i < arrayToSort.length - 1) {
+            if (arrayToSort[i] > arrayToSort[i + 1]) {
+                tmp = arrayToSort[i];
+                arrayToSort[i] = arrayToSort[i + 1];
+                arrayToSort[i + 1] = tmp;
+                i = 0;
             } else {
-                for (int i = 0; i < arrayToSort.length - 1; i++) {
-                    if (arrayToSort[i] < arrayToSort[i + 1]) {
-                        tmp = arrayToSort[i];
-                        arrayToSort[i] = arrayToSort[i + 1];
-                        arrayToSort[i + 1] = tmp;
-                    }
-                }
+                i++;
             }
+
         }
 
         localTimeEnd = LocalTime.now();
@@ -42,11 +36,34 @@ public class Multisort {
     }
 
     public int[] bubbleSortImpr(int[] arrayToSort) {
+
+        int tmp;
+        int sorted = 0;
+        int i = 0, j = 0;
+
+        if (arrayToSort == null) throw new IllegalArgumentException();
+        if (arrayToSort.length == 0) throw new IllegalArgumentException();
+        if (arrayToSort.length == 1) return arrayToSort;
+        j = arrayToSort.length - 1;
+
         localTimeStart = LocalTime.now();
-        //todo: sorting phase
+        while (i < j) {
+            if (arrayToSort[i] > arrayToSort[i + 1]) {
+                tmp = arrayToSort[i];
+                arrayToSort[i] = arrayToSort[i + 1];
+                arrayToSort[i + 1] = tmp;
+            }
+            i++;
+            if (i == j) {
+                j--;
+                i = 0;
+            }
+            if (j == 1) break;
+        }
+
         localTimeEnd = LocalTime.now();
         sortDuration = Duration.between(localTimeStart, localTimeEnd).toMillis();
-        return null;
+        return arrayToSort;
     }
 
     public int[] selectionSort(int[] arrayToSort) {
