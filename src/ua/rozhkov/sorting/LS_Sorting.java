bf174a9b18d3ec91a675e27464825c7aa9e81b14
PsyhoBelka <Fghjrcbvfwbz88>
2019-02-05 20:49:46 +0200

    menu done

--- src/ua/rozhkov/sorting/LS_Sorting.java
+++ src/ua/rozhkov/sorting/LS_Sorting.java
@@ -2,58 +2,65 @@ package ua.rozhkov.sorting;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class LS_Sorting {
 
-    static int[] arrayForSort = new int[1000];
+    static int[] arrayForSort = new int[10];
 
     public static void main(String[] args) {
 
         MultiSort multisort = new MultiSort();
+        int input = 1;
 
-        System.out.println("Select sorting algorithm: ");
-        System.out.println("1. Bubble sort");
-        System.out.println("2. Improved bubble sort");
-        System.out.println("3. Selection sort");
-        System.out.println("4. Insertion sort");
-        System.out.println("5.Merge sort");
-        System.out.println("6. Quick sort");
+        while (input != 0) {
+            show_menu();
+            System.out.print("You choice: ");
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextInt();
+            switch (input) {
+                case 1:
+                    System.out.println("---Bubble sort---");
+                    init_array();
+                    System.out.println(Arrays.toString(multisort.bubbleSort(arrayForSort)));
+                    System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
+                    System.out.println("-----------------");
+                    break;
+                case 2:
+                    System.out.println("---Improvement bubble sort---");
+                    init_array();
+                    System.out.println(Arrays.toString(multisort.bubbleSortImpr(arrayForSort)));
+                    System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
+                    System.out.println("-----------------------------");
+                    break;
+                case 3:
+                    System.out.println("---Selection sort---");
+                    init_array();
+                    System.out.println(Arrays.toString(multisort.selectionSort(arrayForSort)));
+                    System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
+                    System.out.println("--------------------");
+                    break;
+                case 4:
+                    System.out.println("---Insertion sort---");
+                    multisort.insertionSort(arrayForSort);
+                    System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
+                    System.out.println("--------------------");
+                    break;
+                case 5:
+                    System.out.println("---Merge sort---");
+                    multisort.mergeSort(arrayForSort);
+                    System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
+                    System.out.println("----------------");
 
-        System.out.println("---Bubble sort---");
-        init_array();
-        System.out.println(Arrays.toString(multisort.bubbleSort(arrayForSort)));
-        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
-        System.out.println("-----------------");
-
-        System.out.println("---Improvement bubble sort---");
-        init_array();
-        System.out.println(Arrays.toString(multisort.bubbleSortImpr(arrayForSort)));
-        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
-        System.out.println("-----------------------------");
-
-        System.out.println("---Selection sort---");
-        init_array();
-        System.out.println(Arrays.toString(multisort.selectionSort(arrayForSort)));
-        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
-        System.out.println("--------------------");
-
-        System.out.println("---Insertion sort---");
-        multisort.insertionSort(arrayForSort);
-        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
-        System.out.println("--------------------");
-
-        System.out.println("---Merge sort---");
-        multisort.mergeSort(arrayForSort);
-        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
-        System.out.println("----------------");
-
-        System.out.println("---Quick sort---");
-        multisort.quickSort(arrayForSort);
-        System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
-        System.out.println("----------------");
-
-
-//        System.out.println(LocalTime.now());
+                    break;
+                case 6:
+                    System.out.println("---Quick sort---");
+                    multisort.quickSort(arrayForSort);
+                    System.out.println("Time for sorting: " + multisort.sortDuration + "ms");
+                    System.out.println("----------------");
+                    break;
+            }
+        }
     }
 
     public static void init_array() {
@@ -63,5 +70,18 @@ public class LS_Sorting {
         System.out.println("Array to sort:");
         System.out.println(Arrays.toString(arrayForSort));
         System.out.println("...SORTING...");
+    }
+
+    private static void show_menu() {
+        System.out.println();
+        System.out.println("Select sorting algorithm: ");
+        System.out.println("1. Bubble sort");
+        System.out.println("2. Improvement bubble sort");
+        System.out.println("3. Selection sort");
+        System.out.println("4. Insertion sort");
+        System.out.println("5. Merge sort");
+        System.out.println("6. Quick sort");
+        System.out.println("--------------------------");
+        System.out.println("0. Exit");
     }
 }
